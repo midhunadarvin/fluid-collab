44e94ffd6045a9f22a555a166f1162a7ce52e9c9
midhunadarvin <midhunadarvin@gmail.com>
2015-12-12 20:07:42 +0530

    Add production config file

--- config/env/development.js
+++ config/env/development.js
@@ -1,7 +1,6 @@
 module.exports = {
 	// Development configuration options
-	// db: 'mongodb://localhost/fluid-collab',
-	db: 'mongodb://root:password@ds061464.mongolab.com:61464/fluid-collab',
+    db: 'mongodb://localhost/fluid-collab',
 	sessionSecret: 'developmentSessionSecret'
 
 };
---
+++ config/env/production.js
@@ -0,0 +1,6 @@
+module.exports = {
+	// Production configuration options
+	db: 'mongodb://root:password@ds061464.mongolab.com:61464/fluid-collab',
+	sessionSecret: 'productionSessionSecret'
+
+};
