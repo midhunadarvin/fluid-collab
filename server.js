6703e21d393d9b75dd41eccdac5e77e913a36d5f
midhunadarvin <midhunadarvin@gmail.com>
2015-12-12 20:18:20 +0530

    Dynamic port allocation fix for heroku app

--- server.js
+++ server.js
@@ -4,10 +4,14 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var mongoose = require('./config/mongoose'), 
 	express = require('./config/express');
 
+
 var db = mongoose();						
 var app = express();										// create the express application
 
-app.listen(3000);
-module.exports = app;										
-	
-console.log('Server running at http://localhost:3000/');
+app.set('port', (process.env.PORT || 3000));
+
+module.exports = app;			
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});							
