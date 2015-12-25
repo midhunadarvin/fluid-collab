d66d6a6a022dd83c3ba11fc20145dc4f1d8e0b83
midhunadarvin <midhunadarvin@gmail.com>
2015-12-25 23:16:41 +0530

    Add blue color theme to design

--- public/application.js
+++ public/application.js
@@ -2,11 +2,18 @@
 var mainApplicationModuleName = 'FluidCollab';
 
 // We will add the any dependencies to the application
-var mainApplicationModule = angular.module(mainApplicationModuleName, ['ui.router', 'ngMaterial' ,'main','users']);
+var mainApplicationModule = angular.module(mainApplicationModuleName, ['ui.router', 'ngMdIcons','ngMaterial' ,'main','users']);
 
 mainApplicationModule.config(['$locationProvider',function($locationProvider) {
 		$locationProvider.hashPrefix('!');
 }]);
+
+mainApplicationModule.config(function($mdThemingProvider) {
+	    $mdThemingProvider.theme('default')
+		    .primaryPalette('blue')
+		    .accentPalette('orange')
+		    .backgroundPalette('light-blue');
+});
 
 if (window.location.hash === '#_=_') window.location.hash = '#!';
 
