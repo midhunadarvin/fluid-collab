84f3afeda95b128904351fb8901ece148bdd7e7c
Midhun Darvin <midhunadarvin@gmail.com>
2016-02-14 21:08:58 +0530

    load projects from database

--- public/home/controllers/home.client.controller.js
+++ public/home/controllers/home.client.controller.js
@@ -11,14 +11,60 @@ angular.module('home')
 
 	$scope.name = Authentication.user ? Authentication.user.fullName : 'MEAN Application';
 
+  // Initialize Dashboard
   $scope.dashboardInit = function(){
 
       if(Authentication.user)
         $scope.user = Authentication.user;
       else
         $state.go('index.signin');
+
+      $scope.listProjects();
       
   }
+
+  // Save project to database
+  $scope.addProject = function(new_project){
+
+      console.log("Saving project :");
+
+      var project = new Projects(new_project);
+
+      project.$save(function(response) {
+        console.log("Project saved successfully : " + response);
+        new_project = response.project;
+      }, function(errorResponse) {
+        $scope.error = errorResponse.data.message;
+        console.log("Project save unsuccessfull : " + errorResponse.data.message);
+      });
+      
+      $mdDialog.hide(new_project);
+  };
+
+  // Get the list of projects from database
+  $scope.listProjects = function(){
+
+      console.log("Listing projects :");
+
+      $scope.projects = Projects.query();
+      $scope.$apply();
+  };
+
+  // Show dialog box for adding new project
+  $scope.showAddDialog = function(ev) {
+      $mdDialog.show({
+        controller: 'HomeController',
+        templateUrl: '/home/views/dialogs/home.dialog.addProject.html',
+        targetEvent: ev,
+      })
+      .then(function(project) {
+        if(project)
+          $scope.projects.unshift(project);
+      }, function() {
+        $scope.alert = 'You cancelled the dialog.';
+      });
+  };
+
   // Bottom settings popup
 	$scope.showListBottomSheet = function($event) {
 
@@ -34,40 +80,13 @@ angular.module('home')
 	  });
 
   };
-
-  $scope.addProject = function(new_project){
-
-      var project = new Projects(new_project);
-
-      project.$save(function(response) {
-        console.log("Project saved successfully : " + response);
-      }, function(errorResponse) {
-        $scope.error = errorResponse.data.message;
-        console.log("Project save unsuccessfull : " + errorResponse.data.message);
-      });
-      
-      $mdDialog.hide(new_project);
-  };
-
-  $scope.showAdd = function(ev) {
-      $mdDialog.show({
-        controller: 'HomeController',
-        templateUrl: '/home/views/dialogs/home.dialog.addProject.html',
-        targetEvent: ev,
-      })
-      .then(function(project) {
-        //$scope.alert = 'You said the information was "' + answer + '".';
-        if(project)
-          $scope.projects.unshift(project);
-        //$scope.$apply();
-      }, function() {
-        $scope.alert = 'You cancelled the dialog.';
-      });
-    };
-
+  
+  // Hide dialog box
   $scope.hide = function() {
     $mdDialog.hide();
   };
+
+  // Cancel dialog box
   $scope.cancel = function() {
     $mdDialog.cancel();
   };
@@ -107,51 +126,6 @@ angular.module('home')
     }
   ];
 
-  /*$scope.admin = [
-    {
-      link : '',
-      title: 'Trash',
-      icon: 'delete'
-    },
-    {
-      link : 'showListBottomSheet($event)',
-      title: 'Settings',
-      icon: 'settings'
-    }
-  ];*/
 
-
-  $scope.projects = [
-      {
-        title: 'Sample project 1',
-        client: 'Max',
-        time: '3:08PM',
-        leader: "Midhun"
-      },
-      {
-        title: 'Sample project 2',
-        client: 'Vinay',
-        time: '3:08PM',
-        leader: "Midhun"
-      },
-      {
-        title: 'Sample project 3',
-        client: 'Lisa',
-        time: '3:08PM',
-        leader: "Midhun"
-      },
-      {
-        title: 'Sample project 4',
-        client: 'SomeClient',
-        time: '3:08PM',
-        leader: "Midhun"
-      },
-      {
-        title: 'Sample project 5',
-        client: 'SomeClient',
-        time: '3:08PM',
-        leader: "Midhun"
-      }
-  ];
 	
 }]);
